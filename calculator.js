c6366c356549c69a06a26d86e29c2721f9273a17
sudihardi <sudyhardy@gmail.com>
2020-10-14 09:49:02 +0700

    init commit

---
+++ calculator.js
@@ -0,0 +1,16 @@
+// Write multiply, divide, sum, minus function
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function sum(a, b) {
+    return a + b
+}
+
+function minus(a, b) {
+    return a - b
+}
